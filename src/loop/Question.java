e54d73309323610af71b26bb5ad1b1b7d386c891
BMDK9 <qnrakdlzm77@naver.com>
2024-03-07 16:18:26 +0900

    반복문

--- src/loop/Question.java
+++ src/loop/Question.java
@@ -5,6 +5,31 @@ public class Question {
     public static void main(String[] args) {
 
         question1();
+        question2();
+    }
+
+    private static void question2() {
+        int num = 0;
+
+        while (true) {
+            num++;
+
+            if (num%2 == 0) {
+                System.out.println("while num = " + num);
+            }
+
+            if (num == 20) {
+                break;
+            }
+        }
+
+        for (int i = 0; ; i++) {
+            System.out.println("for num = " + (num = i * 2));
+
+            if (num == 20) {
+                break;
+            }
+        }
     }
 
     private static void question1() {
